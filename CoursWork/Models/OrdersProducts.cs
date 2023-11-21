16c724e57397debd518bb65678a02508462cdfd5
AllRaos <rusl.chaban@gmail.com>
2023-11-21 22:29:56 +0200

    kurok6
    ajaja

--- CoursWork/Models/OrdersProducts.cs
+++ CoursWork/Models/OrdersProducts.cs
@@ -1,7 +1,11 @@
-﻿namespace CoursWork.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoursWork.Models
 {
     public class OrdersProducts
     {
+        [Key]
+        public int OrdersProductsId { get; set; }
         public Orders? OrderId { get; set; }
         public Products? ProductId { get; set; }
         public int Quantity { get; set; }
